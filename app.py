52c9443ccafa5478e61d0abacae40afd41365d92
Raj dubey <com.mailuser@gmail.com>
2020-06-12 16:32:23 +0530

    login tested

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request
+import hashlib
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=="GET":
+        #show when default this url is loaded ..
+        return render_template('login.html',a="getr")
+    else:
+        # after user submit his username and password we get to this...
+        email=request.form.get('email',None)
+        password=request.form.get('psw',None)
+        
+        return render_template('login.html',a="post--"+email+"---"+hashlib.sha256(b'{password}').hexdigest())
+
+
+
+
+
+
+
+
+
 if __name__=="__main__":      
     app.run(debug=True)
