52f8f5198c1aa41515a0f854aa27542f7f888700
Raj dubey <com.mailuser@gmail.com>
2020-06-13 14:38:54 +0530

    db structure plus register data being saved now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from retail_banking import app
-from retail_banking import  routes
+from retail_banking import app,routes
+
 if __name__=="__main__":    
     app.run(debug=True)
--- retail_banking/__init__.py
+++ retail_banking/__init__.py
@@ -1,4 +1,3 @@
 from flask import Flask,render_template,url_for,request
-from retail_banking import *
 app=Flask(__name__)
 app.secret_key="123124124nfnefn"
