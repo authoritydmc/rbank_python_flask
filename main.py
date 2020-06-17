683f4fe202931df852ee0426eb3e284609e41f67
Raj dubey <com.mailuser@gmail.com>
2020-06-17 14:38:08 +0530

    possible FIX for import error

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import retail_banking.DATABASES
-from retail_banking.DATABASES import database,customerdb,transactiondb,executive
+import retail_banking.databases
+from retail_banking.databases import database,customerdb,transactiondb,executive
 from retail_banking import app,routes
 
 # print('__file__={0:<35} |\n__name__={1:<20} |\n__package__={2:<20}'.format(__file__,__name__,str(__package__)))
 if __name__=="__main__":    
-    app.run(host='0.0.0.0',debug=True)
+    # app.run(host='0.0.0.0',debug=True)
+    pass
 
