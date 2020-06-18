6439df38aa97283d67056af0103153b45fe14f16
Raj dubey <com.mailuser@gmail.com>
2020-06-18 17:29:26 +0530

    renamed main.py to app.py for starter

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn main:app
+web: gunicorn app:app
 flask: flask run
--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from retail_banking.databases import database,customerdb,transactiondb,executive
 from retail_banking import app,routes
 
 if __name__=="__main__":    
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0')
 
